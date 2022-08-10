94c481e56b752b12568ece7a55454b1530a97d76
Erwin Rosales <look4erwin@gmail.com>
2022-08-09 19:40:44 -0700

    Updated favicon, index, manifest to to reflect Goat Circuits

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './App.css'
-import reportWebVitals from './reportWebVitals';
 
 import Header from './components/Header/Header'
 import Services from './components/Services/Services'
@@ -14,7 +12,7 @@ import Footer from './components/Footer/Footer'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className="app">
+    <div className="index">
       <Header title="Goat Circuits"/>
       <Services />
       <About />
@@ -23,8 +21,3 @@ root.render(
     </div>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
